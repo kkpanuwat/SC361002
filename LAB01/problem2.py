a082e585c39fd5bb63abedb6ce06881b77f45890
kkpanuwat <panuwat_ys@kkumail.com>
2021-07-29 01:57:32 +0700

    lab1 version2

---
+++ LAB01/problem2.py
@@ -0,0 +1,6 @@
+number = int(input("Enter Number : "))
+print(number,"x",1,"=",number*1)
+print(number,"x",2,"=",number*2)
+print(number,"x",3,"=",number*3)
+print(number,"x",4,"=",number*4)
+print(number,"x",5,"=",number*5)
