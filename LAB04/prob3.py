7f5abaa6d8e7b6b53a8d73c1442e3daa6b7a7665
kkpanuwat <panuwat_ys@kkumail.com>
2021-08-02 11:18:04 +0700

    Update LAB04

--- LAB04/prob3.py
+++ LAB04/prob3.py
@@ -1,4 +1,5 @@
 dataset = [1, 3, 5, 7, 9, 11, 21, 25, 31, 55, 101, 105]
 data  =  int(input('Enter value: '))
 # result  = date in dataset
+# print(date,'is in dataset',result)
 print(date,'is in dataset',data in dataset)
