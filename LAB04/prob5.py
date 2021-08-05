601807dab13e0fc02500aa450d62acfb43296a1d
kkpanuwat <panuwat_ys@kkumail.com>
2021-08-05 22:25:56 +0700

    update prob5

--- LAB04/prob5.py
+++ LAB04/prob5.py
@@ -1,6 +1,2 @@
 number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22]
-result = list()
-for i in number:
-    if(i%3==0):
-        result.append(i)
-print('Result: ',result)
+print(number[2::3])
