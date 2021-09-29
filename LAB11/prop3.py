993af779ac37336ac76b0bd35971441bb9700528
kkpanuwat <panuwat_ys@kkumail.com>
2021-09-29 13:28:04 +0700

    update

--- LAB11/prop3.py
+++ LAB11/prop3.py
@@ -6,9 +6,9 @@ def color(data):
             result[i]+=1
         else:
             result.setdefault(i,1)
-    print(dict(sorted(result.items(),key=lambda x:x[0],reverse = True)))
+    return (dict(sorted(result.items(),reverse = True)))
 
 sentence01 = "blue-green-red-red-blue-yellow-black-white-blue-red-green-black-black-blue"
 sentence02 = "blue-blue-blue-blue-red-red-green-green-white-white"
 
-color(sentence01)
+print(color(sentence01))
